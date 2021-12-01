de94545925a743e1b0bd3bd7b04fb1c0c67b89a1
Ricardo <riximuslol@gmail.com>
2021-12-01 23:41:59 +0100

    painting works, working on sharing

--- miniMAL_Project/app/src/main/java/com/example/m335_minimal_app/PaintView.java
+++ miniMAL_Project/app/src/main/java/com/example/m335_minimal_app/PaintView.java
@@ -11,22 +11,35 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class PaintView extends View {
 
     public ViewGroup.LayoutParams params;
     private final Path path = new Path();
     private final Paint brush = new Paint();
 
+    private static Bitmap bitmap = null;
+
+    PaintActivity paintActivity;
+
     public PaintView(Context context) {
         super(context);
 
         brush.setAntiAlias(true);
-        brush.setColor(Color.GREEN);
+        brush.setColor(Color.rgb(51,51,51));
         brush.setStyle(Paint.Style.STROKE);
         brush.setStrokeJoin(Paint.Join.ROUND);
         brush.setStrokeWidth(8f);
 
         params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        //this.setBackgroundColor(Color.MAGENTA);
+    }
+
+    public static Bitmap getBitmap() {
+        return bitmap;
     }
 
     @Override
@@ -45,73 +58,19 @@ public class PaintView extends View {
                  return false;
          }
          postInvalidate();
+
+
          return false;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+
         canvas.drawPath(path,brush);
+        try {
+            bitmap = Bitmap.createBitmap(getWidth(),getHeight(), Bitmap.Config.RGB_565);
+        }catch(Exception e){}
     }
+
+
 }
-
-
-//---------------------------------------------------------------------------------
-// todo Version 2 PaintView
-/*
-    Bitmap mergedBitmap = null;
-    Canvas mergedCanvas = null;
-
-    Bitmap bitmap = null;
-    Canvas canvas = null;
-    int backgroundColor = 0xff000000;
-    Paint defaultPaint = new Paint();
-
-    private Paint brush = new Paint();
-    private Point touchCoor = new Point();
-    private Path path = new Path();
-
-    int red = 51 ,green = 230 ,blue = 51; // 51,51,51
-
-
-    public PaintView(Context context, int width, int height) {
-        super(context);
-        mergedBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        mergedCanvas = new Canvas(mergedBitmap);
-    }
-
-    public void changeBackgroundColor(int newColor){
-        backgroundColor = newColor;
-        invalidate();
-    }
-
-    public void startPaint(int touchX, int touchY) {
-        brush.setAntiAlias(true);
-        brush.setColor(Color.rgb(red,green,blue));
-        brush.setStyle(Paint.Style.STROKE);
-        brush.setStrokeJoin(Paint.Join.ROUND);
-        brush.setStrokeWidth(8f);
-
-        this.touchCoor.x = touchX;
-        this.touchCoor.y = touchY;
-
-        path.moveTo(touchCoor.x, touchCoor.y);
-    }
-
-    public void continuePaint(int touchX, int touchY) {
-        canvas.drawPath(path,brush);
-        this.touchCoor.x = touchX;
-        this.touchCoor.y = touchY;
-        invalidate();
-        // todo control if path can be given in activity
-    }
-
-    private void mergeLayers(){
-        mergedCanvas.drawColor(backgroundColor);
-        mergedCanvas.drawBitmap(mergedBitmap, 0 , 0, defaultPaint);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        mergeLayers();
-        canvas.drawBitmap(mergedBitmap, 0 , 0, defaultPaint);
-    }*/
