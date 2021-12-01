08ee0e6faab0a3c1dae546efb5b5ddd484842de3
Ricardo <riximuslol@gmail.com>
2021-12-01 09:46:17 +0100

    btn_newCanvas switches to PaintActivity

--- miniMAL_Project/app/src/main/java/com/example/m335_minimal_app/MainActivity.java
+++ miniMAL_Project/app/src/main/java/com/example/m335_minimal_app/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.m335_minimal_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btn_newCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_newCanvas = findViewById(R.id.btn_newCanvas);
+
+        ButtonNewCanvas();
+    }
+
+    private void ButtonNewCanvas (){
+        btn_newCanvas.setOnClickListener(view -> {
+            switchActivity();
+        });
+    }
+
+    private void switchActivity(){
+        Intent switchActivity = new Intent(this, PaintActivity.class);
+        startActivity(switchActivity);
     }
 }
